234663e756689002cf6936b3a16715821c7054fe
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2024-12-16 20:25:56 +0530

    Add files via upload

---
+++ JAVA_CHALLENGE/src/com/Dhinesh_Developer/DAY1.java
@@ -0,0 +1,39 @@
+/*
+ * Evolution of computers.
+ * 1.abacus 2500bc  had a mechanical components.
+ * 
+ * 2.ENAIC 1940's  not have a gui. only software enginners can use
+ * 
+ * 3.Gui based 1980's . gui based  invented by steve jobs. had a monitor,keyboard,mouse..
+ * 
+ * 4.modern computers. compact and GUI based.
+ * 
+ *what is an computer.
+ *   is an electronic device that processes the informations and performs a tasks based on instructions.
+ *   it is an collections of hardware and software.
+ *   
+ * 
+ *why computers understand 0's and 1's?
+ *		the microprocessors i created using semiconductors technology.
+ *		semiconductors technology devices are made using up of transistors.
+ *
+ *what is programmer?
+ *		a human being who gives instructions to the CPU is called programmer.
+ *		a collection of instructions is called a programming.
+ *
+ *what is complier?
+ *		it is an software which takes as high level languages as input,line by line converted into
+ *		machine level is called compiler.
+ *   
+ */
+package com.Dhinesh_Developer;
+
+
+public class DAY1 {
+
+	public static void main(String[] args) {
+		System.out.println("Full stack java Developer Challenge. ");
+		// output: Full stack java Developer Challenge.
+	}
+
+}
---
+++ JAVA_CHALLENGE/src/com/Dhinesh_Developer/DAY2.java
@@ -0,0 +1,36 @@
+package com.Dhinesh_Developer;
+
+/*
+ *  DAY 2 Learning.
+ *  
+ *   --------- Features of Java -----------
+ *   1. Simple
+ *   2. Object oriented
+ *   3.Platform Dependent
+ *   4. Portable
+ *   
+ *   C is an structured platform dependent
+ *   c++ is an object oriented 
+ *   
+ *    *=== Java is an object oriented   { platform independent} === *
+ *    
+ *    platform is an combination of hardware and software.
+ *    
+ *    1.PLATFORM DEPENDENT MEANING:
+ *    		compilations and execution have been done on the same os.
+ *    
+ *    2.PLATFORM INDEPENDENT MEANING:
+ *    		compilations and execution runs on any platform with a compatible devices.
+ *    
+ *    3.What is JVM
+ *    		JAVA Virtual machine. converts byte code into machine code.
+ *    
+ *    4. JAVA is invented by Games Gosling and team at sun microsystem company. in 1992
+ */
+
+public class DAY2 {
+	public static void main(String[] args) {
+		System.out.println("Day 2 challenge of Full stack java Developer.");
+	}
+
+}
