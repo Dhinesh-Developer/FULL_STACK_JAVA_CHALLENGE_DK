c0a17a86a6d04f1276d6fa6fb89ee39f31e36331
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2024-12-18 07:58:34 +0530

    Update DAY3.java

--- JAVA_CHALLENGE/src/com/Dhinesh_Developer/DAY3.java
+++ JAVA_CHALLENGE/src/com/Dhinesh_Developer/DAY3.java
@@ -2,13 +2,17 @@ package com.Dhinesh_Developer;
 
 /* DAY 3
  *  OBJECT ORIENTATION
- *  
+ *  orientation :
+ 	way of lokking or perspective.
  *  Object orientation :
  *  	object orientation is an perspective of looking at the world as 
  *  	a collection of object.
  *  
- *  classes are imaginary.
- *  object are real.
+ *  classes (type) are imaginary. class is an state or property [has part]  -> data types.
+ *  object (action) are real.  object is an behaviour   [does part]  -> methods.
+
+   to create a object.
+     Activatiing a jvm  to create a object   using a  "new " keyword.
  */
 
 class car{
