33cf255ca967a242c71ffdfe58fe31f9ce5b6b98
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2024-12-19 07:13:19 +0530

    Add files via upload

---
+++ JAVA_CHALLENGE/src/com/Dhinesh_Developer/DAY4.java
@@ -0,0 +1,32 @@
+package com.Dhinesh_Developer;
+/*
+ *  MAIN METHOD
+ *   	os gives the control of execution to the main method 
+ *   the main should present inside the class.
+ *   System.out(stream) is called as pipe.
+ *   
+ *   println for new line   and the  print for same line.
+ *   
+ *   public -> to visible, for main is present.identify by os.
+ *   static -> no need to create a object for class.
+ *   void ->  no return statement.
+ *   
+ *   to compile in cmd.
+ *   stage 1. javac Demo.java  -> to compile high level to byte code . (compiler) does this part.
+ *   stage 2. javac Demo -> to to convert byte code to machine level code. (JVM) does this part
+ *   
+ *   System.out.println(args[o]); we can pass the arguments to main methods.while executing.
+ *   
+ *   valid syntax:
+ *   	-> public static void main(String...args);
+ *   	-> static public void main(String[] args);
+ */
+public class DAY4 {
+
+	public static void main(String[] args) {
+		System.out.println("Main methods Concept.");
+//		System.out.println(args[0]); // to pass cmd in main method also.
+//		System.out.println(args[1]);
+	}
+
+}
